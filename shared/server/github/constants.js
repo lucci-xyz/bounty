7371201ea4de214ca1acc7b9c7275581ba263b1e
natalie-a-1 <natalie.a.hill-1@ou.edu>
2025-12-03 12:31:16 -0600

    Update BRAND_SIGNATURE in constants.js to reflect new branding. Changed the signature to include the BountyPay icon and adjusted the HTML structure for improved visual consistency.

--- shared/server/github/constants.js
+++ shared/server/github/constants.js
@@ -4,8 +4,8 @@ import { getLinkHref } from '@/shared/config/links';
 export const FRONTEND_BASE = CONFIG.frontendUrl.replace(/\/$/, '');
 export const CTA_BUTTON = `${FRONTEND_BASE}/buttons/create-bounty.svg`;
 export const OG_ICON = `${FRONTEND_BASE}/icons/og.png`;
-export const BRAND_SIGNATURE = `_ <img src="${FRONTEND_BASE}/buttons/brand-signature.svg" alt="BountyPay signature" width="220" height="40" style="display:inline-block; vertical-align:middle;" />_`;
-
+export const BRAND_SIGNATURE =
+`_By BountyPay <img src="${OG_ICON}" alt="BountyPay Icon" width="23" height="23" />_`;
 export const BADGE_BASE = getLinkHref('assets', 'badgeBase');
 export const BADGE_LABEL_COLOR = '111827';
 export const BADGE_STYLE = 'for-the-badge';
